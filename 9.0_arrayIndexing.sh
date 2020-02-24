a22366b18ade18924f9e96ff350990d6487687c0
bharath02 <fee0228@gmail.com>
2020-02-24 19:20:37 +0530

    array changes

--- 9.0_arrayIndexing.sh
+++ 9.0_arrayIndexing.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 array_name=('bharath' 'Naresh' 'suvam' "Shiva")
-echo ${array_name[0]} ${array_name[1]} ${array_name[2]} ${array_name[3]} 
+array_name[5]='hariak'
+echo ${array_name[0]} ${array_name[1]} ${array_name[2]} ${array_name[3]}
+echo ${array_name[@]} 
 
 array_name=('bharath' 'Naresh' 'suvam' "Shiva")
 echo "checking @ : " ${array_name[@]}
 echo "checking * : " ${array_name[*]}
+echo "checking * : " ${!array_name[@]}
+echo "checking * : " ${#array_name[@]}
+unset array_name[3]
+echo ${array_name[@]}
