cbdb4d95d43f190249e040343f7fcf0df39eff3e
bharath02 <fee0228@gmail.com>
2020-02-24 19:19:31 +0530

    modifed addition of two number

--- 6.0_addNumber.sh
+++ 6.0_addNumber.sh
@@ -4,5 +4,15 @@ b=20
 c=$((a + b))
 echo $c
 
-val='expr 2+2'
+val=$(expr $a + $b)
 echo " $val "
+
+echo "12.5+5" | bc
+c=12.5
+d=12 
+
+echo "$c+$d" | bc
+
+echo " scale=2;sqrt($b)" | bc -l
+echo " scale=2;3^3" | bc
+
