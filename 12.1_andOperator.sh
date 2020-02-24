a2f180cc9b18e34fe15a45e1bf95751a4a726faf
bharath02 <fee0228@gmail.com>
2020-02-24 19:23:19 +0530

    AND && Operator

---
+++ 12.1_andOperator.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+age=25
+if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]
+then 
+	echo "valid age : $age"
+   else 
+	echo "Not valid age : $age"
+fi
+
+if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]
+then 
+        echo "valid age : $age"
+   else 
+        echo "Not valid age : $age"
+f
